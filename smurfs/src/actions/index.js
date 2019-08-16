0a435e7b6cb82b44b68e5e36605953a7f738c500
MichaelBaynon <baynonmichael@gmail.com>
2019-08-16 12:41:57 -0400

    wip

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START"
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS"
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE"
+
+export const getSmurfs = () => dispatch => {
+    
+dispatch({type: FETCH_SMURFS_START})
+axios.get('http://localhost:3333/smurfs')
+.then( res =>  console.log("RESPONSE DATA", res.data)  || dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data}))
+.catch(error => dispatch({type:FETCH_SMURFS_FAILURE, payload: error.response}))    
+}
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,39 @@
+import {FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS ,
+    FETCH_SMURFS_FAILURE} from '../actions'
+
+const initialState = {
+smurfs: null,
+error: '',
+loading: false
+}
+
+const reducer = (state=initialState, action) => {
+   
+    switch(action.type) {
+        case FETCH_SMURFS_START:
+            return {
+                ...state,
+                error: '',
+                loading: true
+            }
+            case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                loading: false,
+                smurfs: action.payload
+            }
+            case FETCH_SMURFS_FAILURE:
+                    return {
+                        ...state,
+                        error: action.payload,
+                        loading: false,
+                        smurfs: null
+                    }
+        default:
+            return state
+    }
+}
+
+export default reducer
